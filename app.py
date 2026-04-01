a644de928512ffa2a3512e293bee225641f5ae83
Keerthana Namulametu <88832768+KEERTHANANAMULAMETU@users.noreply.github.com>
2026-04-01 12:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from tensorflow.keras.models import load_model
 
-model = load_model("model.h5",compile=False)
+model = load_model("churn_model.keras",compile=False)
 
 with open('scaler.pkl','rb') as file:
     scaler=pickle.load(file)
